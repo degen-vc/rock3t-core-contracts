dbe712d3401e3f8b4a5a8846f4e91b195daaee6f
Elena Derymova <elenaderymova@gmail.com>
2020-12-23 16:30:22 +0200

    Secondary address in FeeDistributor added

--- contracts/FeeDistributor.sol
+++ contracts/FeeDistributor.sol
@@ -2,10 +2,17 @@
 pragma solidity ^0.7.1;
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/math/SafeMath.sol";
 
 contract FeeDistributor is Ownable {
+    using SafeMath for uint;
+
     IERC20 public R3Ttoken;
-    address liquidVault;
+
+    address public liquidVault;
+    address public secondaryAddress;
+
+    uint256 public secondaryAddressShare;
 
     bool public initialized;
 
@@ -17,15 +24,32 @@ contract FeeDistributor is Ownable {
         _;
     }
 
-    function seed(address r3t, address vault) public onlyOwner {
+    function seed(address r3t, address vault, address _secondaryAddress, uint256 _secondaryAddressShare) public onlyOwner {
         R3Ttoken = IERC20(r3t);
         liquidVault = liquidVault;
+        secondaryAddress = _secondaryAddress;
+        secondaryAddressShare = _secondaryAddressShare;
 
         initialized = true;
     }
 
     function distributeFees() public seeded {
-        uint fees = R3Ttoken.balanceOf(address(this));
-        R3Ttoken.transfer(liquidVault,fees);
+        uint balance = R3Ttoken.balanceOf(address(this));
+        
+        uint256 fees;
+
+        if (secondaryAddressShare > 0) {
+            fees = secondaryAddressShare.mul(balance).div(100);
+
+            require(
+                R3Ttoken.transfer(secondaryAddress, fees),
+                "FeeDistributor: transfer to the secondary address failed"
+            );
+        }
+
+        require(
+            R3Ttoken.transfer(liquidVault, balance.sub(fees)),
+            "FeeDistributor: transfer to LiquidVault failed"
+        );  
     }
 }
