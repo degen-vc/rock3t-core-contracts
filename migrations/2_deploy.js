17cda933e8a9218ee5dadf49cfdf1b961ca58e4a
Elena Derymova <elenaderymova@gmail.com>
2021-01-12 11:56:27 +0200

    deployment script added

--- migrations/2_deploy.js
+++ migrations/2_deploy.js
@@ -1,8 +1,10 @@
 require('dotenv').config();
 
+const FeeApprover = artifacts.require('FeeApprover');
 const FeeDistributor = artifacts.require('FeeDistributor');
 const RocketToken = artifacts.require('RocketToken');
 const LiquidVault = artifacts.require('LiquidVault');
+const SlidingWindowOracle = artifacts.require('SlidingWindowOracle');
 
 const { 
     UNISWAP_FACTORY, 
@@ -12,26 +14,39 @@ const {
 } = process.env;
 
 module.exports = async function (deployer, network, accounts) {
-    const fee = 0 // 1%;
+    const fee = 0
     const blackHoleFee = 10 // 1%;
-    const rocketFee = 10;
+    const defaultWindowSize = 86400 // 24 hours
+    const defaultGranularity = 24 // 1 hour each
 
     if (network === 'development') {
         return;
     }
+
+    await deployer.deploy(FeeApprover);
+    const feeApproverInstance = await FeeApprover.deployed();
+    await pausePromise('Fee Approver');
+
     await deployer.deploy(FeeDistributor);
     const feeDistributorInstance = await FeeDistributor.deployed();
-    await pausePromise('fee Distributor');
+    await pausePromise('Fee Distributor');
 
-    await deployer.deploy(RocketToken, 10, feeDistributorInstance.address, UNISWAP_ROUTER, UNISWAP_FACTORY);
+    await deployer.deploy(RocketToken, feeDistributorInstance.address, feeApproverInstance.address, UNISWAP_ROUTER, UNISWAP_FACTORY);
     const rocketTokenInstance = await RocketToken.deployed();
     await pausePromise('RocketToken');
 
     await deployer.deploy(LiquidVault);
     const liquidVaultInstance = await LiquidVault.deployed();
-    await pausePromise('liquidity vault');
+    await pausePromise('Liquidity Vault');
+
+    await deployer.deploy(SlidingWindowOracle, UNISWAP_FACTORY, defaultWindowSize, defaultGranularity);
+    const uniswapOracle = await SlidingWindowOracle.deployed();
     
     const uniswapPair = await rocketTokenInstance.tokenUniswapPair();
+
+    await pausePromise('seed fee approver');
+    await feeApproverInstance.initialize(rocketTokenInstance.address, UNISWAP_FACTORY, UNISWAP_ROUTER, liquidVaultInstance.address);
+    await feeApproverInstance.unPause();
 
     await pausePromise('seed fee distributor');
     await feeDistributorInstance.seed(
@@ -47,7 +62,8 @@ module.exports = async function (deployer, network, accounts) {
       blackHoleFee,
       UNISWAP_ROUTER,
       uniswapPair,
-      TREASURY
+      TREASURY,
+      uniswapOracle.address
     );
 
 }
